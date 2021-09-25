7c83173eb8a002a647dc944673435df62d9ddaf4
Dedvukaj <58040998+Dedvukaj@users.noreply.github.com>
2021-09-25 17:22:48 -0400

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,6 @@
 // Simple C++ program to display "Hello World" 
 // Header file for input output functions 
+// Author Nicholas Dedvukaj <Dedvukaj@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
